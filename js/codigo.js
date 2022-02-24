c38477b624b3513a05880c4c5b413898a85ae86b
Thainara <thai.cds1@gmail.com>
2022-02-23 22:39:42 -0300

    correção do espaco ' ' e do (Z)

--- js/codigo.js
+++ js/codigo.js
@@ -1,8 +1,9 @@
 var alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var btnEnviar = document.getElementById("BtnEnviarCifra");
 var btnDescifrar = document.getElementById("descifrar");
+const espaco = ' ';
 
-//C = (POSICAO +CHAVE) %26
+//😜😜😜😈 CIFRA DE CESAR 😈😜😜😜
 
 
 //chamando a funcao, metodozin novo, é nois
@@ -13,7 +14,7 @@ btnEnviar.addEventListener("click", function(e){
     cifrar();
 });
 
-//Pegando a palavra e transfornando em caixa alta
+//METODO PARA TRANSFORMAR EM CAIXA ALTA
 function pegandoPalavra(){
     var palavra = document.getElementById("palavraDigitada").value;
     var chave = document.getElementById("chave").value;
@@ -23,23 +24,31 @@ function pegandoPalavra(){
     return [palavraCaixaAlta, chave];
 }
 
-
 function cifrar(){
     var funcaoPegarPalavra = this.pegandoPalavra();
     var palavraDigitada = funcaoPegarPalavra[0];
     var palavraCifrada = '';
-    var chave = funcaoPegarPalavra[1];
+    var chave = parseInt(funcaoPegarPalavra[1]);
     
-
+    
     for (var i = 0; i < palavraDigitada.length; i++ ){ //percorrendo tamanho da palavra digitada
-
+        if (palavraDigitada[i] == espaco){
+            palavraCifrada = palavraCifrada + espaco;    
+            
+        }else{
             var letra = palavraDigitada[i];
             var posicaoNoAlfabeto = alfabeto.lastIndexOf(letra); //posicao de cada letra no alfabeto
-            palavraCifrada = palavraCifrada + alfabeto[parseInt(posicaoNoAlfabeto) + parseInt(chave)];    
 
-            console.log(palavraCifrada);          
-            document.getElementById("resposta").value = palavraCifrada;            
-        
+            if ( (posicaoNoAlfabeto + chave) > 25)  {
+                var restoSubtracao = (parseInt(posicaoNoAlfabeto) + chave) - 25;
+                palavraCifrada = palavraCifrada + alfabeto[restoSubtracao - 1];    
+            }else{            
+                palavraCifrada = palavraCifrada + alfabeto[parseInt(posicaoNoAlfabeto) + chave];    
+            }
+
+                   
+        }    
+        document.getElementById("resposta").value = palavraCifrada; 
     }
 }
 
@@ -55,18 +64,27 @@ function descifrar(){
     var funcaoPegarPalavra = this.pegandoPalavra();
     var palavraDigitada = funcaoPegarPalavra[0];
     var palavraCifrada = '';
-    var chave = funcaoPegarPalavra[1];
+    var chave = parseInt(funcaoPegarPalavra[1]);
     
-
     for (var i = 0; i < palavraDigitada.length; i++ ){ //percorrendo tamanho da palavra digitada
-
+        if (palavraDigitada[i] == espaco){
+            palavraCifrada = palavraCifrada + espaco; 
+        }else{
             var letra = palavraDigitada[i];
-            
             var posicaoNoAlfabeto = alfabeto.lastIndexOf(letra); //posicao de cada letra no alfabeto
-            palavraCifrada = palavraCifrada + alfabeto[parseInt(posicaoNoAlfabeto) - parseInt(chave)];    
 
-            console.log(palavraCifrada);          
+            console.log("aqui que a magica acontece de novo!😈");
+            if ( (posicaoNoAlfabeto - chave) < 0)  {
+                var restoSubtracao = posicaoNoAlfabeto - chave;
+                palavraCifrada = palavraCifrada + alfabeto[2 + restoSubtracao];    
+            }else{
+                palavraCifrada = palavraCifrada + alfabeto[parseInt(posicaoNoAlfabeto) - chave];    
+            }
+        }
+                   
             document.getElementById("resposta").value = palavraCifrada;            
         
     }
 }
+
+
